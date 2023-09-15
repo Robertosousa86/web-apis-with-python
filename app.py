72d3f5110c1e1392bfb4384c89051981e52818f2
Roberto Sousa <robertosousa186@gmail.com>
2023-09-14 23:18:08 -0300

    Complete task

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, jsonify, request
 # Intitialise the app
 app = Flask(__name__)
 
+# app.run(debug=True)
+
 # Define what the app does
 @app.get("/greet")
 def index():
@@ -14,4 +16,16 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    return jsonify("TODO")
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+
+    if not first_name and not last_name:
+        return jsonify({'status': 'error'})
+    elif first_name and not last_name:
+        response = {'data': f'Hello {first_name}!!!!!'}
+    elif not first_name and last_name:
+        response = {'data': f'Hello {last_name}!'}
+    else:
+        response = {'data': f'Is your name {first_name} {last_name}'}
+
+    return jsonify(response)
