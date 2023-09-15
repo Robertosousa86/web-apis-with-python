9a5e54b3c506ba2fb874040071836081c89042e1
Roberto Sousa <robertosousa186@gmail.com>
2023-09-15 18:43:39 -0300

    Complete task

--- app.py
+++ app.py
@@ -1,22 +1,29 @@
 from flask import Flask, request, render_template, redirect
 
+"""
+Logic of application
+
+1. Render the homepage with a search box and submit button
+2. User submission is sent to API as a GET request
+3. The API backend captures the GET request and parses the query
+4. The API then redirects to Google with appropriate querystring to search for . 
+"""
+
 app = Flask(__name__)
 
-@app.get("/")
+@app.get('/')
 def index():
-    """
-    TODO: Render the home page provided under templates/index.html in the repository
-    """
-    return "TODO"
+    return render_template('index.html')
 
-@app.get("/search")
+@app.get('/search')
 def search():
-	"""
-	TODO:
-	1. Capture the word that is being searched
-	2. Seach for the word on Google and display results
-	"""
-    return "TODO"
+    args = request.args.get('q')
+    return redirect(f'https://google.com/search?q={args}')
 
-if __name__ == "__main__":
+@app.get('/search')
+def get_lucky():
+    args = request.args.get('s')
+    return redirect(f'https://www.google.com/webhp?s={args}')
+
+if __name__ == '__main__':
     app.run()
